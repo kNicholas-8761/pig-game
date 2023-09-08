f2dd289e540cd6c4db90e5ad01b6af6c112edb23
kbrown <kamauri205@gmail.com>
2023-09-08 21:21:18 +1000

    change image to match dice

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const dice = document.querySelector('.dice');
+const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -10,16 +10,18 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 
-dice.classList.add('hidden');
+diceEl.classList.add('hidden');
 
 //Rolling dice funtionality
 
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
 
-  Math.trunc(Math.random() * 6 + 1);
-
+  let dice = Math.trunc(Math.random() * 6 + 1);
+  console.log(dice);
   // 2.Display dice
-  dice.classList.remove('hidden');
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
   // 3. Check for rolled 1: if true, switch to next player
 });
