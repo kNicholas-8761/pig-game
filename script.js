dddb8d3ab5359976e33e8eafcc99f673aae1f70d
kbrown <kamauri205@gmail.com>
2023-09-10 22:35:16 +1000

    added the reset button logic

--- script.js
+++ script.js
@@ -11,20 +11,45 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const scores = [0, 0];
+// const scores = [0, 0];
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// score0El.textContent = 0;
+// score1El.textContent = 0;
+// diceEl.classList.add('hidden');
+// let currentScore = 0;
+// let activePlayer = 0;
+// let playing = true;
+
+let playing, currentScore, activePlayer, scores;
+
+const init = function () {
+  scores = [0, 0];
+
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  playerEl0.classList.remove('player--winner');
+  playerEl1.classList.remove('player--winner');
+  playerEl0.classList.add('player--active');
+  playerEl1.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
   currentScore = 0;
-  current0El.textContent = 0;
+  // current0El.textContent = 0;
   playerEl0.classList.toggle('player--active');
   playerEl1.classList.toggle('player--active');
 };
@@ -59,8 +84,9 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //2. Check if player score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
+      diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
@@ -68,10 +94,10 @@ btnHold.addEventListener('click', function () {
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
     } else {
+      //3. Switch to the next player
       switchPlayer();
     }
   }
-
-  //3. Switch to the next player
-  switchPlayer();
 });
+
+btnNew.addEventListener('click', init);
